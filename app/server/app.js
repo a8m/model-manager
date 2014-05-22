b09cb58bf2a8997186b4b36abbcf2b9563fe86fd
Ariel Mashraki <ariel@codeoasis.com>
2014-05-22 08:01:04 +0300

    remove unnessacery files(add them to git ignore)

--- app/server/app.js
+++ app/server/app.js
@@ -1,20 +1,27 @@
 var http = require('http'),
   express = require('express'),
   fs = require('fs'),
+  namescpace = require('express-namespace'),
+  resource = require('express-resource'),
   app = express();
 
 var iniparser = require('iniparser'),
   config = iniparser.parseSync('config.ini');
 
 //load the route handlers
-var routes = require('./routes'),
-  user = require('./routes/users');
+var routes = require('./routes')(app);
 
 //configuration json file for dev/prod environment
 var configFile = require('./config.json')[app.get('env')];
 
+
+//load the resourceful route handler --- awesome!! handle routing alone
+app.resource('users', require('./resource/users.js'));
+
+
 app.set('view engine', 'jade');
 app.set('views', './views');
+
 // app.set('env', 'development');
 // or pass the environment in the kick-start:
 // NODE_ENV=production node app.js
@@ -28,37 +35,10 @@ if('production' == app.get('env')){
   console.log('development mode');
 }
 
-app.get('/', function(req, res){
-  res.render('index', {title:config.title, message:config.message});
-});
 
-app.get('/user/:id', function(req, res){
-  res.send('user id: ' + req.params.id);
-});
-
-app.get('/say-hello', function(req, res){
-  res.render('hello');
-});
-
-app.get('/test', routes.index);
-
-app.get('/use-next',
-  function(req, res, next){
-    res.set('X-one', 'foo');
-    next();
-  },
-  function(req, res, next){
-    res.set('X-two', 'bar');
-    next();
-  },
-  function(req, res, next){
-    res.send('open debugger and watch headers');
-  }
-);
-
-app.post('/', function(req,res){
-  res.send('/ POST OK');
-});
+//app.get('/', function(req, res){
+//  res.render('index', {title:config.title, message:config.message});
+//});
 
 
 http.createServer(app).listen(config.port, function(){
--- app/server/routes/users.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Ariel Mashraki
- */
-
-exports.list = function(req, res){
-  res.send('foo', 'bar', 'baz');
-};
